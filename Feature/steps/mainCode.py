c48a90b18d264b9093def659a4c538f6aa0026cd
Sarthak Saxena A <sarthaksax11406@gmail.com>
2024-07-04 20:09:13 +0530

    Completed

--- Feature/steps/mainCode.py
+++ Feature/steps/mainCode.py
@@ -1,9 +1,7 @@
 import json
 import os
-import time
-import webbrowser
 
-from behave import given, when, then, step
+from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
